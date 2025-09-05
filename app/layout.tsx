45e95d712c3b00e5b6d10542f26279c9ebce3a1a
pedro <pedroyhan@gmail.com>
2025-09-04 21:04:05 -0300

    Change web metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,8 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'PYH',
+  description: 'One Piece Price Comparator',
 }
 
 export default function RootLayout({
